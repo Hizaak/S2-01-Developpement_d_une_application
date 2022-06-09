33f01442c5a008e53d6462542112f816495242bc
Alexandre Maurice <hizaaknewton@gmail.com>
2022-06-09 10:48:15 +0200

    v5
    Ajout d'un temps limité

--- chifoumi/presentation.h
+++ chifoumi/presentation.h
@@ -1,6 +1,7 @@
 #ifndef PRESENTATION_H
 #define PRESENTATION_H
 
+#include <QTimer>
 #include <QObject>
 #include "modele.h"
 
@@ -22,19 +23,45 @@ public:
      * BUT: Créer une nouvelle partie de chifoumi, et reinitialise la page.
      */
 
+    void pauseTimer();
+    /*
+     * BUT: Met en pause le timer
+     */
+
+    unsigned int getTempsEcoule();
+    /*
+     * BUT: Retourne le temps deja écoulé
+     */
+
+    unsigned int getTempsRestant();
+    /*
+     * BUT: Retourne le temps restant
+     */
+
     Chifoumi* getModele();
     ChifoumiVue* getVue();
     void setModele(Chifoumi *m);
     void setVue(ChifoumiVue *v);
 
-    char verificationFinPartie();
+    bool timerIsActive();
+    /*
+     * BUT: Indique si le timer est actif (true si actif)
+     */
 
-    unsigned int pointMax=5;
-
+private slots:
+    void majTimer();
 
 private:
-    Chifoumi *_leModele; // partie de chifoumi
-    ChifoumiVue *_laVue; // partie de chifoumi
+    Chifoumi *_leModele;
+    ChifoumiVue *_laVue;
+    QTimer* timer;
+
+    unsigned int lgTimer = 30;
+    unsigned int valeurTimer;
+    unsigned int pointMax=5;
+
+    char verificationFinPartie();
+
 
 };
 
